182d8dc620338becaff73dcb2b83b0d3aafa2ab4
Can Savcı <highcsavci@gmail.com>
2023-08-26 23:42:15 +0300

    python main.py

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import yaml
 
 class Config(object):
     def __init__(self):
-        with open("config.yaml", "r", encoding="utf-8") as cf:
+        with open("config/config.yaml", "r", encoding="utf-8") as cf:
             self.config = yaml.safe_load(cf)
 
     def __new__(cls):
